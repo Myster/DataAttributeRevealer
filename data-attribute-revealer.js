d9e8305683159503b1a20a6246cf313ec6767263
Murray Roke <murray@roke.co.nz>
2012-10-15 11:26:54 +1300

    Initial Commit (copied from my GIST)

---
+++ data-attribute-revealer.js
@@ -0,0 +1,25 @@
+// D.A.R. Data Attribute Revealer by Murray Roke
+// =============================================
+// todo: load jQuery (currently requires jquery to already exists)
+
+var $body = $('body');
+var $reveals = $('.__d_a_r_item');
+if($reveals.length)
+{
+	$reveals.remove();
+}
+else
+{
+	$('body *:not(iframe *,script *)').each(function(){ 
+		var $el = $(this);
+		var j=''; 
+		try{ 
+			j =JSON.stringify($el.data()); 
+		}catch(e){} 
+		if(j.length>2) 
+		{
+			var $r = $('<div class="__d_a_r_item" style="background-color:pink;">'+j+'</div>');
+			$el.prepend($r);
+		} 
+	});
+}
